a49061ce3c3aa9d404c6fa50120312e5a4eb9bdb
idea-your-father <1957476540@qq.com>
2021-05-25 12:54:42 +0800

    '修改一下目录'

---
+++ main_test001.go
@@ -0,0 +1,22 @@
+//package main
+package goUtil
+//
+//func main() {
+//	//var str string = "hello_world_App"
+//	//s :=util.Upfirst( (str))
+//	//1tln(util.Md5([]byte(str)))
+//	// fmt.Println(util.GetCurrentProjectPath())
+//	var h map[string]interface{}
+//	h = map[string]interface{} {"hello":"world"}
+//	h["hello"] = "1"
+//	fmt.Println(h["hello"])
+//	for k,v := range h {
+//		fmt.Println(k)
+//		fmt.Println(v)
+//	}
+//	//fmt.Println(str)
+//	//fmt.Println(s)
+//	//fmt.Println(util.LowFirst(s))
+//	//
+//	//fmt.Println(util.GetRandomString(6))
+//}
